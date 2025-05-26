81d5435ae4211f111b35caf46f71867fed5a003c
rht-21 <iamrht21@gmail.com>
2025-05-26 13:02:37 +0530

    style: Correct padding in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <main className="relative">
+    <main className="relative px-4">
       <span className="absolute top-1/2 left-1/2 translate-[-50%] h-full aspect-square w-auto rounded-full bg-aqyre-primary -z-10 blur-[200px] opacity-40 dark:opacity-30" />
       <Hero />
     </main>
